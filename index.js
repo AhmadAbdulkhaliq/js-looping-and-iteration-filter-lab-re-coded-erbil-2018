a4503a81730dc21d0306553c00d8a7c2886c7aa6
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-07-26 22:01:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching (array , name){
+  return array.filter(function(drivername){
+    return drivername.toUpperCase()===name.toUpperCase();
+  });
+}
+
+function toLowerCase(array,letter){
+  return array.filter(function(drivername){
+    let length=letter.length;
+    return drivername.slice(0,length)===letter;
+  })
+}
